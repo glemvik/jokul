3bef4fd33eb4900ddf963a9e7d7acf79424815f9
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-31 11:27:57 +0000

    Deploy fremtind/jokul to github.com/fremtind/jokul.git:gh-pages

--- component---src-pages-profile-icon-tsx-6a3be1df48588b9aa7ed.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[35],{257:function(e,n,r){"use strict";r.r(n);var l=r(0),t=r.n(l),o=r(267),s=r.n(o),a=r(266),i=r(268),u="## Ikoner\n\nVi bruker ikoner sparsomt. De ikonene vi har valgt å ha, er funksjonelle, for eksempel til noen av Fremtinds fagsystemer og til fellesfunksjoner som å legge til en fil eller lagre.\n";function f(){return t.a.createElement(a.d,null,t.a.createElement(s.a,{renderers:i.b,source:u}))}r.d(n,"default",function(){return f})}}]);
-//# sourceMappingURL=component---src-pages-profile-icon-tsx-6a3be1df48588b9aa7ed.js.map
---
+++ component---src-pages-profile-logo-tsx-f2bfdc1c71a66ce09918.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[38],{260:function(n,e,r){"use strict";r.r(e);var t=r(0),o=r.n(t),u=r(268),a=r.n(u),c=r(267),s=r(269),d="## Logo\n";function i(){return o.a.createElement(c.d,null,o.a.createElement(a.a,{renderers:s.b,source:d}))}r.d(e,"default",function(){return i})}}]);
+//# sourceMappingURL=component---src-pages-profile-logo-tsx-f2bfdc1c71a66ce09918.js.map
